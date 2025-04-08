91373568b6603bac9cbcafc32bce82a14eb84659
akirayamaa <157995976+akirayamaa@users.noreply.github.com>
2025-04-08 13:41:33 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,14 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, options) => {
+    console.log(`Comparing files: ${filepath1} and ${filepath2}`);
+    if (options.format) {
+      console.log(`Format: ${options.format}`);
+    }
+  });
 
 program.parse();
